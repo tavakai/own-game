6926e77d61b087245f7d390b82d8948075f35c47
AbdurRohman <minsultanov@mail.ru>
2023-01-20 11:01:58 +0300

    release

---
+++ client/src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="dropdown">
+      <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+        Dropdown button
+      </button>
+
+    </div>
+  );
+}
+
+export default App;
